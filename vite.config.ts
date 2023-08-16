723d408614fe9a81a5131b720f7ed0fb11f8f648
Abinash Karmakar <codeAbinash@gmail.com>
2023-08-16 19:12:18 +0530

    Fixed million not found error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import million from 'million/compiler'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/routine',
-  plugins: [million.vite(), react()],
+  plugins: [react()],
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
